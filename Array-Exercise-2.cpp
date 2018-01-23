0e84513d8dbd0d77dfc77a264d338f140e5ecbfa
Fawwaz Fahmi <fwzfhmy@gmail.com>
2018-01-23 10:36:42 +0800

    exercise updated

--- Array-Exercise-2.cpp
+++ Array-Exercise-2.cpp
@@ -1,6 +1,6 @@
-//this is not a complete program. please finish the program. thank you.
-
-
+// MUHAMMAD FAWWAZ FAHMI BIN MOHD KAFII
+// A17DW0064
+// SIR ADIB HENSEM
 #include <iostream>
 using namespace std;
 
@@ -8,25 +8,13 @@ int mark[5]={19,10,8,17,9};
 int main()
 {
 	int x;
-	//insert different value to the third element
-	//tukar third element tu dengan value lain
 	mark[3]=24;
 
 	cout << "Please enter the new third element for array " << endl;
 	cin >> mark[3];
 
-	cout << "Please enter the xth" << endl;
-	cin >> mark[x];
-
-
-	//get user input and insert sub the third element
-	//tukar the third element yang baru ditukar tu dengan input dari user
-	//mark[x];
 	cout << "The first element of the array is " << mark[0] <<endl;
-	//print the first element of the array
-	//mark[];
-	cout << "The ith element of the array is " << mark[x-1];
-	//print the ith element of the array
-	//mark[x]
 
+	cout << "The last element of the array is " << mark[4];
+	return 0;
 }
