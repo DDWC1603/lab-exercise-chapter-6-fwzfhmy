dfae1df9399a640165397e5e5049bd0f6cf48853
Fawwaz Fahmi <fwzfhmy@gmail.com>
2018-01-23 10:42:12 +0800

    exercise updated

--- String-Exercise-3.cpp
+++ String-Exercise-3.cpp
@@ -1,24 +1,24 @@
-//use the string compare function to compare input string with the setup string
-//make a check with the string password(if else)
-
+// MUHAMMAD FAWWAZ FAHMI BIN MOHD KAFII
+// A17DW0064
+// SIR ADIB HENSEM
 #include <iostream>
+#include <string.h>
+using namespace std;
 
 int main()
 {
 	char strPswd[80];
 
 	cout<<"Enter password: ";
-	//get the strPswd
-	
-	//make an if else that check the password(strPswd)
-	//(string compare function (strPswd,"Check")) 
-		
-		cout<<"invalid password "<<endl;
-	
-	 
-		cout<<"Logged in "<<endl;
+		cin >> strPswd;
 
-	
-
+	if(strcmp(strPswd,"fawwaznice")!=0)
+		{
+			cout<<"invalid password "<<endl;
+		}
+	else
+		{
+			cout<<"Logged in "<<endl;
+		}
 	return 0;
 }
